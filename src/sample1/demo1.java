b0a7448156d9ea7b9bae4f10149d5eddec372c6a
Hp <Hp@LAPTOP-R80MKK4A>
2022-08-17 10:33:21 +0530

    [kshitija] : add new project

---
+++ src/sample1/demo1.java
@@ -0,0 +1,36 @@
+package sample1;
+
+public class demo1 {
+
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("hiiii");
+		
+		System.out.println("hiiii");
+		System.out.println("hiiii");
+		System.out.println("hello");
+		System.out.println("hello");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
